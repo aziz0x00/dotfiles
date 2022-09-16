893e624cd3f40e6485ca21a8687dbb2de0aa2880
Aziz KHATTATT <aziz0x00@github.com>
2022-09-16 00:23:43 +0000

    updated first-conf.sh

--- first-conf.sh
+++ first-conf.sh
@@ -1,6 +1,59 @@
-useradd aziz -Gwheel,video
-mkdir -p ~/.config/{dunst,zathura} ~/.histfiles
-ln -s ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
-ln -s ~/.cache/wal/zathurarc ~/.config/zathura/zathurarc
+#!/bin/bash
 
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.local/share/zsh/plugins/fsh/
+myname=aziz
+
+
+# helper
+function info() {
+  echo '[i]' $@
+}
+
+# stage 1
+function sys_conf() {
+
+  info 'adding user'
+  useradd $myname -m
+  usermod $myname -aG $myname,wheel
+
+  info 'user created! set password:'
+  passwd $myname
+
+  info 'installing yay'
+  git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -sri && cd .. && rm yay -rf 2>/dev/null
+
+  info 'installing pkglist.txt'
+  yay -S `cat pkglist.txt`
+
+  info 'copying to /etc'
+  cp -r etc/* /etc -rv
+
+  usermod $myname -aG wireshark,docker,input,video
+}
+
+
+# stage 2
+function user_conf() {
+
+  mkdir -p ~/.config/{dunst,zathura} ~/.histfiles
+  ln -s ~/.cache/wal/dunstrc ~/.config/dunst/dunstrc
+  ln -s ~/.cache/wal/zathurarc ~/.config/zathura/zathurarc
+  cp -r .??* ~
+
+  git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.local/share/zsh/plugins/fsh/
+
+  info 'installing python-pkhlist.txt'
+  pip install -r python-pkglist.txt
+}
+
+
+[[ $UID -ne 0 ]] && echo 'ERROR: not root' && exit 1
+
+info 'starting system-wide configs'
+sys_conf
+
+su $myname
+
+info 'starting user-wide configs'
+user_conf
+
+info 'FINISHED :D'
